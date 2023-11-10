5a772390cbd40eb998e363ec2d25cfc586a28a2b
Aanchal Tailwal <tailwalaanchal@gmail.com>
2023-11-10 20:44:56 +0530

    Add & update

--- src/array/addAndUpdate.java
+++ src/array/addAndUpdate.java
@@ -1,6 +1,6 @@
 package array;
 
-public class ArrayUtil {
+public class addAndUpdate {
 	public void printArray(int[] arr) {
 		int n = arr.length;
 		for(int i = 0; i < n; i++) {
@@ -10,11 +10,19 @@ public class ArrayUtil {
 	}
 	public void arrayDemo() {
 		int[] myArray = new int[5]; //declaring and initializing the array
-		printArray(myArray);
+		// printArray(myArray);
+		myArray[0] = 5; //adding values to ARRAY
+		myArray[1] = 1;
+		myArray[2] = 8;
+		myArray[3] = 52;
+		myArray[4] = 21;
+		myArray[2] = 10000;  //UPDATING VALUE
+		//myArray[5] = 52;     //index out of bound 
+		printArray(myArray); 
 		
 	}
 	public static void main (String[] args) {
-		ArrayUtil arrUtil = new ArrayUtil();
+		addAndUpdate arrUtil = new addAndUpdate();
 		arrUtil.arrayDemo();      //calling the array demo method
 	}
 
